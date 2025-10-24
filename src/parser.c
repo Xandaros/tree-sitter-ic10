001bb09197c72b87c099132fbc5c27235cca3aca
Martin Zeller <mz.bremerhaven@gmail.com>
2025-10-24 16:31:04 +0200

    Bump version

--- src/parser.c
+++ src/parser.c
@@ -27607,8 +27607,8 @@ TS_PUBLIC const TSLanguage *tree_sitter_ic10(void) {
     .max_reserved_word_set_size = 0,
     .metadata = {
       .major_version = 0,
-      .minor_version = 2,
-      .patch_version = 0,
+      .minor_version = 6,
+      .patch_version = 2,
     },
   };
   return &language;
